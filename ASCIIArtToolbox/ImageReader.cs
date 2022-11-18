de2701b588ff6a3dee7e0171b9d1561d22e522c8
rikdgd <rik.dgd@gmail.com>
2022-11-18 23:55:05 +0100

    Added image reader class setup

---
+++ ASCIIArtToolbox/ImageReader.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+namespace ASCIIArtToolbox
+{
+    public class ImageReader
+    {
+        private string imagePath;
+
+        public ImageReader(string imagePath)
+        {
+            this.imagePath = imagePath;
+        }
+
+        public Image loadImage()
+        {
+            Image image = Image.FromFile(imagePath);
+            return image;
+        }
+    }
+}
